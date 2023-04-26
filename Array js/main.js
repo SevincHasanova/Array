e31744be1b570527e767ee05b765b4b05ab92bf4
SevincHasanova <127186047+SevincHasanova@users.noreply.github.com>
2023-04-26 10:01:39 +0400

    Add files via upload

---
+++ Array js/main.js
@@ -0,0 +1,6 @@
+// Array-a dərsdə keçdiyimizə uyğun remove funksiyası yazın. Bu method Arrayın sonuncu elementini silsin.
+alert();
+const meyvearray = ["ciyelek", "moruq", "boyurtken", "qarpiz"];
+meyvearray.length = meyvearray.length - 1;
+console.log(meyvearray);
+
